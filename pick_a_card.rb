ce079c50c3c1cd52cf4b4978c8e7b42de7955d09
Bambukin <119507077+Bambukin@users.noreply.github.com>
2022-12-02 17:16:10 +0530

    Update

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,5 +3,5 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
 
-# Поменял коментарий дважды
+# Рандомная карта
 puts "#{values.sample} of #{suits.sample}"
