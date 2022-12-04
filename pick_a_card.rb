9402597542169ed2d05f943569e698cf6f2f1900
Bambukin <vadimsorella@gmail.com>
2022-12-05 00:36:59 +0530

    symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,7 +1,7 @@
 
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
 
 # Рандомная карта!
 puts "#{values.sample} of #{suits.sample}"
