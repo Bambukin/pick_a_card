40968a10feab14fd7a3dfc7537760409c96f1167
Bambukin <vadimsorella@gmail.com>
2022-12-04 23:54:01 +0530

    Update

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,5 +3,5 @@
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
 
-# Рандомная карта
+# Рандомная карта!
 puts "#{values.sample} of #{suits.sample}"
